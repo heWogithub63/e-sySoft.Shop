1c59db299db01a845a3e9aa984622a3b0fdd9a58
Heinrich Worring <worrhe@gmail.com>
2026-01-03 07:36:10 +0100

    next version

--- JavaScripts/SecondHandBookShop.js
+++ JavaScripts/SecondHandBookShop.js
@@ -959,8 +959,11 @@ function createBlButton(id,text,name) {
 
                if (Currency === '')
                    window.open('mailto:'+event.target.name+'?subject=Interest on your Book(s)&body='+body);
-               else 
-                   eMail.send(event.target.name, 'Interest on your Book(s)', body);
+               else {
+
+                   console.dir(event.target.name);
+                   sendMail.sendTo(event.target.name, 'Interest on your Book(s)', body);
+               }
 
             };
 
